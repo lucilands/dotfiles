7e435c6863de1963ae42e8ffcffa7c6b9ca942a1
Lucius Landschulze <lucland278@gmail.com>
2026-01-10 14:37:50 +0100

    Add folke/Trouble.nvim to nvim config

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if grep -qEi "(Microsoft|WSL)" /proc/version; then
+  echo "export TMX_ROOT_DIR=/mnt/c" >> ~/.zshrc
+else
+  echo "export TMX_ROOT_DIR=/" >> ~/.zshrc
+fi
+
 mv ~/.tmux.conf ~/.tmux.conf.bak 
 ln -s $(pwd)/.tmux.conf /home/$USER/.tmux.conf
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,8 @@ local map = vim.keymap.set
 
 map("n", "<Tab>", ":bnext<CR>", {noremap = true, silent = true})
 map('n', '<S-Tab>', ':bprevious<CR>', { noremap = true, silent = true })
+
+map("n", "<C-Left>",  "<C-w>h", { silent = true })
+map("n", "<C-Down>",  "<C-w>j", { silent = true })
+map("n", "<C-Up>",    "<C-w>k", { silent = true })
+map("n", "<C-Right>", "<C-w>l", { silent = true })
