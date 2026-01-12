be7f2f3d3a71b9a5de4ae9b05c72b55608c4d884
Lucius Landschulze <lucland278@gmail.com>
2026-01-12 09:23:26 +0100

    Add vsnip

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,4 +1,4 @@
 return {
 	"hrsh7th/nvim-cmp",
-	dependencies = {"hrsh7th/cmp-nvim-lsp", "neovim/nvim-lspconfig"}
+	dependencies = {"hrsh7th/cmp-nvim-lsp", "neovim/nvim-lspconfig", "hrsh7th/vim-vsnip", "hrsh7th/vim-vsnip-integ"}
 }
